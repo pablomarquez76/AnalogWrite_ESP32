f64e8a27104de09aaa25aaf127d39fffd7fa7870
pablomarquez76 <92705318+pablomarquez76@users.noreply.github.com>
2023-06-03 20:39:16 -0300

    Create servoTest.ino

---
+++ examples/servoTest.ino
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <analogWrite.h>
+
+#define PIN_SERVO 0
+
+void setup() {
+  // Set resolution for a specific pin
+  pinMode(PIN_SERVO, OUTPUT);
+}
+
+void loop() {
+ // Move servo using angles
+ analogServo(PIN_SERVO, 0);
+ delay(1000);
+ analogServo(PIN_SERVO, 180);
+ delay(1000);
+ // Move servo using milliseconds
+ analogServo(PIN_SERVO, 600);
+ delay(1000);
+ analogServo(PIN_SERVO, 2400);
+ delay(1000);
+}
