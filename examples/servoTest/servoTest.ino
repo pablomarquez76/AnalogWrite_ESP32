3cf1da60384fa18767cdc8d650b8adbe74fd0f40
Pablo Marquez <92705318+pablomarquez76@users.noreply.github.com>
2023-06-25 18:08:42 -0300

    Update servoTest.ino added dettach example

--- examples/servoTest/servoTest.ino
+++ examples/servoTest/servoTest.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <analogWrite.h>
 
-#define PIN_SERVO 0
+#define PIN_SERVO 22
 
 void setup() {
   // Set resolution for a specific pin
@@ -9,14 +9,16 @@ void setup() {
 }
 
 void loop() {
- // Move servo using angles
- analogServo(PIN_SERVO, 0);
- delay(1000);
- analogServo(PIN_SERVO, 180);
- delay(1000);
- // Move servo using milliseconds
- analogServo(PIN_SERVO, 600);
- delay(1000);
- analogServo(PIN_SERVO, 2400);
- delay(1000);
+  // Move servo using angles
+  analogServo(PIN_SERVO, 0);
+  delay(1000);
+  analogServo(PIN_SERVO, 180);
+  delay(1000);
+  // Move servo using milliseconds
+  analogServo(PIN_SERVO, 600);
+  delay(1000);
+  analogServo(PIN_SERVO, 2400);
+  delay(1000);
+  analogServo(PIN_SERVO, 4000);  // >=4000 dettach
+  delay(4000);
 }
