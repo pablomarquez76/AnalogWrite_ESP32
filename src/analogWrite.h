f7679ffef06236c5595df0aff3e4414fc095c00a
Pablo Marquez <92705318+pablomarquez76@users.noreply.github.com>
2023-07-16 18:53:06 -0300

    Added explanations on usage and definitions

--- src/analogWrite.h
+++ src/analogWrite.h
@@ -12,6 +12,27 @@ In analogServo function values are treated as follows:
             0 - 180                             value (treated as degrees)
             181 - 3999                          value (treated as microseconds)
             4000 and up                             pin output LOW
+            
+* This group/channel/timmer mapping is for information only;
+* the details are handled by lower-level code
+*
+* LEDC Chan to Group/Channel/Timer Mapping
+** ledc: 0  => Group: 0, Channel: 0, Timer: 0
+** ledc: 1  => Group: 0, Channel: 1, Timer: 0
+** ledc: 2  => Group: 0, Channel: 2, Timer: 1
+** ledc: 3  => Group: 0, Channel: 3, Timer: 1
+** ledc: 4  => Group: 0, Channel: 4, Timer: 2
+** ledc: 5  => Group: 0, Channel: 5, Timer: 2
+** ledc: 6  => Group: 0, Channel: 6, Timer: 3
+** ledc: 7  => Group: 0, Channel: 7, Timer: 3
+** ledc: 8  => Group: 1, Channel: 0, Timer: 0
+** ledc: 9  => Group: 1, Channel: 1, Timer: 0
+** ledc: 10 => Group: 1, Channel: 2, Timer: 1
+** ledc: 11 => Group: 1, Channel: 3, Timer: 1
+** ledc: 12 => Group: 1, Channel: 4, Timer: 2
+** ledc: 13 => Group: 1, Channel: 5, Timer: 2
+** ledc: 14 => Group: 1, Channel: 6, Timer: 3
+** ledc: 15 => Group: 1, Channel: 7, Timer: 3
 
 */
 
