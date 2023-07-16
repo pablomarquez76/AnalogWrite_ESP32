d7b8648322d3f55d3cc234409527581e025015ee
Pablo Marquez <92705318+pablomarquez76@users.noreply.github.com>
2023-07-16 18:09:51 -0300

    Added some comments on usage

--- src/analogWrite.h
+++ src/analogWrite.h
@@ -1,3 +1,24 @@
+/*
+
+All pin numbers are allowed, but pins 2,4,12-13,16-19,21-23,25-27,32-33 are recommended.
+Remember that pins 0,1,3,5,14-15 are HIGH at boot, pins 1,3 are also used for Serial 
+communication, pins 34-36 and 39 are input only pins.
+
+In tone function the maximum frequency is 7800Hz and maximum duration is 2000ms (2s).
+
+In analogServo function values are treated as follows:
+            Value                                   Becomes
+            -----                                   -------
+            0 - 180                             value (treated as degrees)
+            181 - 3999                          value (treated as microseconds)
+            4000 and up                             pin output LOW
+
+*/
+
+#ifndef analogWrite_ESP32_h
+#define analogWrite_ESP32_h
+
+
 #include <Arduino.h>
 
 typedef struct analog_write_channel {
@@ -23,3 +44,5 @@ void analogServo(int8_t pin, uint32_t value);
 void tone(int8_t pin, uint32_t freq);
 void tone(int8_t pin, uint32_t freq, uint32_t duration);
 void notone(int8_t pin);
+
+#endif
